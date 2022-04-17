80cf4c31fb1e8a2104bd17cdd506c11a180e8b79
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-04-18 02:26:37 +0600

    set about and blogs

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>abooooout</h2>
+            <img src="https://i.ibb.co/mJJngMG/120243316-1010765549367127-3698105278526712518-n-1.jpg" alt="" />
+            <h2>ABU ISA</h2>
         </div>
     );
 };
