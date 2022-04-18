325244de13d13fd6f7f9916299d645a23fe184f1
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-04-18 22:48:11 +0600

    blog and about

--- src/Pages/Doctor/Doctor.js
+++ src/Pages/Doctor/Doctor.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Doctor = () => {
     return (
         <div id='dentist'>
-            <div className='d-flex mt-5 p-2 bg-info'>
-                <div><img style={{"margin-right":'10px'}} className='rounded' src="https://i.ibb.co/vhhSnQY/doctor1.jpg" alt="" /></div>
-                <div style={{"margin-top":'50px'}}>
+            <div className=' d-flex mt-5 p-2 bg-info'>
+                <div><img style={{"margin-right":'10px', height:'150px'}} className='rounded' src="https://i.ibb.co/vhhSnQY/doctor1.jpg" alt="" /></div>
+                <div>
                     <h1 className='fs-2 text-dark'>Dr.Anagha Menen <span className='fs-6 text-danger'>M.D, pH.D</span></h1>
                 <p className='fs-3 ' >MBBS,BDS , MDS - Periodontology and Oral Implantology, 16 Years Experience</p>
                 </div>
